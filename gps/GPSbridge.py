42706bb7bb7c2b12d4a92f4d2c125e64f0b4763f
jaron0211 <jaron0211@gmail.com>
2023-11-01 05:29:40 +0800

    update.

--- gps/GPSbridge.py
+++ gps/GPSbridge.py
@@ -65,53 +65,48 @@ class _GPS_unit():
                 self.freq = 1/(time.time() - self.frequence_timer + 0.00001)
                 self.frequence_timer = time.time()
     
-    def NMEA_GPRMC(self, msg):
-        print(msg)
-        if len(msg) < 12 or msg[0] != "GPRMC":
-            self.gps_time = 0
-            self.status = ""
-            self.latitude = 0
-            self.latitude_hemisphere = 0
-            self.longitude = 0
-            self.longitude_hemisphere = 0
-            self.speed = 0
-            self.course = 0
-            self.date = 0
+    def NMEA_GPRMC(self, msgs):
+        print(msgs)
+        for mes in msgs:
+            msg = mes.split(',')
+            if len(msg) < 12 or msg[0] != "GPRMC":
+                continue
+
+            # Extract relevant fields
+            time = msg[1]
+            status = str(msg[2])
+            latitude = msg[3]
+            latitude_hemisphere = msg[4]
+            longitude = msg[5]
+            longitude_hemisphere = msg[6]
+            speed = msg[7]
+            course = msg[8]
+            date = msg[9]
+
+            # Check if the GPS fix is valid
+            if status != "A":
+                return None
+
+            # Convert latitude and longitude from NMEA format to decimal degrees
+            latitude_degrees = float(latitude[:2]) + float(latitude[2:]) / 60
+            if latitude_hemisphere == "S":
+                latitude_degrees = -latitude_degrees
+
+            longitude_degrees = float(longitude[:3]) + float(longitude[3:]) / 60
+            if longitude_hemisphere == "W":
+                longitude_degrees = -longitude_degrees
+
+            self.gps_time = msg[1]
+            self.status = str(msg[2])
+            self.latitude = latitude_degrees
+            self.latitude_hemisphere = msg[4]
+            self.longitude = longitude_degrees
+            self.longitude_hemisphere = msg[6]
+            self.speed = msg[7]
+            self.course = msg[8]
+            self.date = msg[9]
+
             return
-
-        # Extract relevant fields
-        time = msg[1]
-        status = str(msg[2])
-        latitude = msg[3]
-        latitude_hemisphere = msg[4]
-        longitude = msg[5]
-        longitude_hemisphere = msg[6]
-        speed = msg[7]
-        course = msg[8]
-        date = msg[9]
-
-        # Check if the GPS fix is valid
-        if status != "A":
-            return None
-
-        # Convert latitude and longitude from NMEA format to decimal degrees
-        latitude_degrees = float(latitude[:2]) + float(latitude[2:]) / 60
-        if latitude_hemisphere == "S":
-            latitude_degrees = -latitude_degrees
-
-        longitude_degrees = float(longitude[:3]) + float(longitude[3:]) / 60
-        if longitude_hemisphere == "W":
-            longitude_degrees = -longitude_degrees
-
-        self.gps_time = msg[1]
-        self.status = str(msg[2])
-        self.latitude = latitude_degrees
-        self.latitude_hemisphere = msg[4]
-        self.longitude = longitude_degrees
-        self.longitude_hemisphere = msg[6]
-        self.speed = msg[7]
-        self.course = msg[8]
-        self.date = msg[9]
 
 
        
